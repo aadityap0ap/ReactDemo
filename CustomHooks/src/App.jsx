ca1266a133b22f2930f19c3716ed5e2878db8fb3
aadityap0ap <adityabharti450ap@gmail.com>
2026-04-05 22:34:12 +0530

    CustomHooks S1 ForuseFetch hook Project Intialised

--- CustomHooks/src/App.jsx
+++ CustomHooks/src/App.jsx
@@ -1,32 +1,10 @@
 import { useState } from "react";
 
-
-function useCounter(){
-  const [count,setCount] = useState(0);
-  function increaseCount(){
-    setCount(count+1)
-  }
-  return {
-    count : count,
-    increaseCount : increaseCount
-  }
-}
-
 function App(){
   return (
     <div>
-      <Counter />
-      <Counter />
-      <Counter />
+      hii there
     </div>
   )
 }
-
-function Counter(){
-const {count,increaseCount} = useCounter();
-  return <div>
-    <button onClick={increaseCount}>Increase{count}</button>
-  </div>
-}
-
 export default App;
