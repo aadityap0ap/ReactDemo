e4420c4742303c7bbbaf21e2f6e998e34f11068a
aadityap0ap <adityabharti450ap@gmail.com>
2026-04-08 18:01:48 +0530

    CustomHooks UsePrev Hook completed

--- CustomHooks/src/App.jsx
+++ CustomHooks/src/App.jsx
@@ -1,22 +1,38 @@
 import { useEffect, useState } from "react";
 import { useFtech } from "./hooks/useFetch";
+import { usePrev } from "./hooks/usePrev";
 
 function App(){
-  const [currentPost,SetCurrentPost ] = useState(1);
-  const { finalData ,loading } = useFtech("https://jsonplaceholder.typicode.com/posts/1" + currentPost);
+  // const [currentPost,SetCurrentPost ] = useState(1);
+  // const { finalData ,loading } = useFtech("https://jsonplaceholder.typicode.com/posts/1" + currentPost);
   
-  if(loading){
-    return <div>
-      Loading......
-    </div>
-  }
+  // if(loading){
+  //   return <div>
+  //     Loading......
+  //   </div>
+  // }
+
+  // return (
+  //   <div>
+  //     <button onClick={() => SetCurrentPost(1)}>1</button>
+  //     <button onClick={() => SetCurrentPost(2)}>2</button>
+  //     <button onClick={() => SetCurrentPost(3)}>3</button>
+  //     {JSON.stringify(finalData)};
+  //   </div>
+  // )
+
+  const [state,setState] = useState(0);
+  const prev = usePrev(state);
 
   return (
     <div>
-      <button onClick={() => SetCurrentPost(1)}>1</button>
-      <button onClick={() => SetCurrentPost(2)}>2</button>
-      <button onClick={() => SetCurrentPost(3)}>3</button>
-      {JSON.stringify(finalData)};
+      <p>{state}</p>
+      <button
+      onClick={()=>{
+        setState((curr) => curr + 1)
+      }}
+      >click me</button>
+      <p>The prev state was{prev}</p>
     </div>
   )
 }
--- CustomHooks/src/hooks/usePrev.js
+++ CustomHooks/src/hooks/usePrev.js
@@ -1,3 +1,11 @@
-export function usePrev(){
-    
+import { useEffect, useRef } from "react";
+
+export function usePrev(value){
+    const ref = useRef();
+
+    useEffect(()=>{
+        ref.current = value;
+    },[value]);
+
+    return ref.current;
 }
