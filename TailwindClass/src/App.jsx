3e8c93662ced8aaeea8c53174c7356ca42720970
aadityap0ap <adityabharti450ap@gmail.com>
2026-04-12 12:39:06 +0530

    Used grid in Tailwind

--- TailwindClass/src/App.jsx
+++ TailwindClass/src/App.jsx
@@ -1,9 +1,9 @@
 export default function App() {
   return (
-    <div className="flex justify-center ">
-      <div>Child 1</div>
-      <div>Child 2</div>
-      <div>Child 3</div>
+    <div className="grid grid-cols-12">
+      <div className="bg-red-800 col-span-4">Child 1</div>
+      <div className="bg-red-500 col-span-6">Child 2</div>
+      <div className="bg-red-200 col-span-2">Child 3</div>
     </div>
   );
 }
