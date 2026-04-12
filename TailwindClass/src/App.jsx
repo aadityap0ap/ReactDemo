5bf263d244e273f9bd134911cfc4d183c6560753
aadityap0ap <adityabharti450ap@gmail.com>
2026-04-12 12:33:59 +0530

    used Flex in Tailwind

--- TailwindClass/src/App.jsx
+++ TailwindClass/src/App.jsx
@@ -1,3 +1,9 @@
-export function App(){
-  
+export default function App() {
+  return (
+    <div className="flex justify-center ">
+      <div>Child 1</div>
+      <div>Child 2</div>
+      <div>Child 3</div>
+    </div>
+  );
 }
