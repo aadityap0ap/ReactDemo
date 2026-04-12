7924689a201bfca075d512d0284f3d2f5af3bcf2
aadityap0ap <adityabharti450ap@gmail.com>
2026-04-12 12:59:19 +0530

    Learned and used Responsiveness

--- TailwindClass/src/App.jsx
+++ TailwindClass/src/App.jsx
@@ -1,9 +1,10 @@
 export default function App() {
   return (
-    <div className="grid grid-cols-12">
+    <div className="sm:grid grid-cols-12">
       <div className="bg-red-800 col-span-4">Child 1</div>
       <div className="bg-red-500 col-span-6">Child 2</div>
       <div className="bg-red-200 col-span-2">Child 3</div>
     </div>
+    // <div className="md:bg-green-800 sm:bg-blue-300 bg-red-900">Hello</div>
   );
 }
